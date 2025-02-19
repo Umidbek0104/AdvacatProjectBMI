81a72a52e156d308cd8424b9a2bef14a036a2e75
umidmadraimov89@gmail.com <umidmadraimov89@gmail.com>
2025-02-19 10:33:51 +0500

    Role installed

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'advacat']);
+        Role::create(['name' => 'user']);
+    }
+}
